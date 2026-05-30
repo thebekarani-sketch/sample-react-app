41660ea47958a0ee63e58617b71e455ff6bea307
Thebekarani <thebekarani@gmail.com>
2026-05-30 23:04:41 +0530

    add items to list

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 // https://react.dev/reference/react/hooks
 function App() {
   var [shoppingList, setShoppingList] = useState([])
-  var test = useState([])
-  
+  var shoppingItemInputRef = useRef(null)
+
+  var onAddItem = (event) => {
+    console.log(event)
+    event.preventDefault()
+    setShoppingList([...shoppingList, shoppingItemInputRef.current.value])
+    shoppingItemInputRef.current.value = ''
+  }
 
   return (
     <div>
       <h1>Shopping List Application</h1>
-      <input placeholder='Enter shopping list item'/>
-      <button onClick={() => setShoppingList([...shoppingList, 'banana'])}>add item</button>
+      <form>
+        <input ref={shoppingItemInputRef} placeholder='Enter shopping list item'/>
+        <button onClick={onAddItem}>add item</button>
+      </form>
+      
       <ol>
         {shoppingList.map((shoppingItem) => <li>{shoppingItem}</li>)}
       </ol>
